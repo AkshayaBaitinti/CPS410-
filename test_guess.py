4eddc54d3ee1f2f4aa28ff11b369e87e894f1ce5
gsishir1104 <sishirgottumukkala@gmail.com>
2025-10-01 10:49:54 -0400

    Add files via upload

---
+++ test_guess.py
@@ -0,0 +1,19 @@
+import unittest
+from Main import check_guess
+
+    
+class TestGuessingGame(unittest.TestCase):
+    def test_guess_too_low(self):
+        result = check_guess(25, 60)
+        self.assertEqual(result, "Too low!")
+
+    def test_guess_too_high(self):
+        result = check_guess(85, 40)
+        self.assertEqual(result, "Too high!")
+
+    def test_guess_correct(self):
+        result = check_guess(50, 50)
+        self.assertEqual(result, "Correct!")
+
+if __name__ == "__main__":
+    unittest.main()
